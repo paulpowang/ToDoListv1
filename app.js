fa7c015bb9dbebbe0b8828425d12efc147a7ac92
PoHung Wang <wang.po.hung@gmail.com>
2021-09-16 19:50:44 -0700

    add ToDoList

--- app.js
+++ app.js
@@ -2,9 +2,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+let items = [];
 app.get("/", function (req, res) {
-  res.send("Hello World!!");
+  let today = new Date();
+
+  const option = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+  let day = today.toLocaleDateString("en-US", option);
+
+  res.render("list", {
+    kindOfDay: day,
+    newListItems: items.length > 0 ? items : ["No items"],
+  });
+});
+
+app.post("/", function (req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
